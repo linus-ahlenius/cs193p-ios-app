bb82881a907c5320d1b12ea55ad6ab7e8d57e5f8
Linus Ahlenius <linus.ahlenius@bontouch.com>
2021-11-29 21:45:00 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Linus Ahlenius on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
