0c289f10ef258a9dcb5ea15cc823932d578ed4ca
Linus Ahlenius <linus.ahlenius@bontouch.com>
2021-12-12 15:03:53 +0100

    Add cards, add/remove buttons and grid view
    This commit basically represent lecture 1 and 2 in the stanford course
    CS193p 2021. https://cs193p.sites.stanford.edu/

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,21 +1,77 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by Linus Ahlenius on 2021-11-29.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗", "🚕", "🏍", "🚁", "🛩", "🚀", "🛶", "⛵️", "🚙", "🚌", "🚎", "🏎", "🚜", "🚛", "🚚", "🛴", "🛵", "🚲", "🚑", "🚒", "🚓"]
+    @State var emojiCount = 7
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+        }
+        .padding(.horizontal)
+    }
+    var remove: some View {
+        Button{
+            if emojiCount > 0 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    var add : some View {
+        Button{
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 10)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {isFaceUp.toggle()}
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+.previewInterfaceOrientation(.landscapeLeft)
+            ContentView()
+                .preferredColorScheme(.light)
+        }
     }
 }
--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -1,10 +1,3 @@
-//
-//  MemorizeApp.swift
-//  Memorize
-//
-//  Created by Linus Ahlenius on 2021-11-29.
-//
-
 import SwiftUI
 
 @main
