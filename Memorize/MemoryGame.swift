b35519c1e386129a8463785c4335deb32b5253ce
Linus Ahlenius <linus.ahlenius@bontouch.com>
2021-12-27 13:19:39 +0100

    Revert "Use class instead of struct"
    This reverts commit 998ee3b166a90d0eb44965ebeef5f0120ab02b91.

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -1,77 +1,69 @@
 import Foundation
 
-class MemoryGame<CardContent> where CardContent: Equatable {
-    private(set) var cards: [Card] = []
+struct MemoryGame<CardContent> where CardContent: Equatable {
+    private(set) var cards: Array<Card> = []
 
-    private var firstChosenCard: Card?
-    private var secondChosenCard: Card?
+    private var indexOfTheFirstChosenCard: Int?
 
-    func choose(_ card: Card) {
-        if !isValidChoice(card) {
-            return
-        }
-
-        card.isFaceUp = true
-
-        if firstChosenCard == nil {
-            firstChosenCard = card
-            return
-        } else { // TODO: Remove else since early return?
-            secondChosenCard = card
-
-            update_match_fields()
-            update_score()
-            update_shown_cards()
-            unset_chosen_cards()
-        }
-    }
-
-    private func isValidChoice(_ card: Card) -> Bool {
-        !card.isFaceUp && !card.isMatched
-    }
-
-    private func update_match_fields() {
-        if firstChosenCard!.content == secondChosenCard!.content {
-            firstChosenCard!.isMatched = true
-            secondChosenCard!.isMatched = true
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched
+        {
+            if let potentialMatchIndex = indexOfTheFirstChosenCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheFirstChosenCard = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheFirstChosenCard = chosenIndex
+            }
+            cards[chosenIndex].isFaceUp.toggle()
         }
     }
 
-    private func update_score() {
-        // TODO: Implement later
-    }
-
-    private func update_shown_cards() {
-        firstChosenCard!.hasBeenShown = true
-        secondChosenCard!.hasBeenShown = true
-    }
-
-    private func unset_chosen_cards() {
-        firstChosenCard = nil
-        secondChosenCard = nil
-    }
-
-    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent){
         cards = Array<Card>()
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = createCardContent(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: pairIndex * 2))
+            cards.append(Card(content: content, id: pairIndex * 2 + 1))
         }
         cards.shuffle()
     }
 
-    init() {
-    }
+    init() {}
 
-    class Card: Identifiable {
+    struct Card: Identifiable {
         var isFaceUp: Bool = false
         var isMatched: Bool = false
-        var hasBeenShown: Bool = false
         var content: CardContent
 
-        init(content: CardContent) {
-            self.content = content
-        }
+        var id: Int
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
