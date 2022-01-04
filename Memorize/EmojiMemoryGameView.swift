2136d9696f5b2a1c4fee6454715bff99008a43e4
Linus Ahlenius <linus.ahlenius@bontouch.com>
2022-01-04 18:19:49 +0100

    After lecture 6, add Pie and AspectVGrid

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -4,17 +4,19 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var game: EmojiMemoryGame
 
     var body: some View {
-        VStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(game.cards) { card in
-                        CardView(card).aspectRatio(2 / 3, contentMode: .fit).onTapGesture {
-                            game.choose(card)
-                        }
+        AspectVGrid(items: game.cards, aspectRatio: 2 / 3) { card in
+            if card.isMatched && !card.isFaceUp {
+                Rectangle().opacity(0)
+            } else {
+                CardView(card)
+                    .padding(4)
+                    .onTapGesture {
+                        game.choose(card)
                     }
-                }
-            }.foregroundColor(.red)
-        }.padding(.horizontal)
+            }
+
+        }.foregroundColor(.red)
+            .padding(.horizontal)
     }
 }
 
@@ -33,6 +35,9 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Pie(startAngle: Angle(radians: -CGFloat.pi / 2), endAngle: Angle(radians: 0.0))
+                        .padding(4)
+                        .opacity(0.5)
                     Text(card.content).font(font(in: geometry.size))
                 } else if card.isMatched {
                     shape.opacity(0)
@@ -50,7 +55,7 @@ struct CardView: View {
     private struct DrawingConstants {
         static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.8
+        static let fontScale: CGFloat = 0.75
     }
 }
 
@@ -60,10 +65,10 @@ struct ContentView_Previews: PreviewProvider {
         let game = EmojiMemoryGame()
         Group {
             EmojiMemoryGameView(game: game)
-                    .preferredColorScheme(.dark)
-                    .previewInterfaceOrientation(.landscapeLeft)
+                .preferredColorScheme(.dark)
+                .previewInterfaceOrientation(.landscapeLeft)
             EmojiMemoryGameView(game: game)
-                    .preferredColorScheme(.light)
+                .preferredColorScheme(.light)
         }
     }
 }
